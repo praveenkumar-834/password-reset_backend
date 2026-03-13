b7ea817f928610bcb6cb02802eb0e4f94c5082a8
praveenkumar-834 <praveenuu34@gmail.com>
2026-03-13 10:50:25 +0530

    port listening change

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(cors())
 
 app.use("/api/auth",authRoutes)
 
-app.listen(5000,()=>{
-console.log("Server running")
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+console.log("Server running ${PORT}")
 })
