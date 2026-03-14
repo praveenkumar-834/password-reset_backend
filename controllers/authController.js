a848cdbefefca8313e5808699dc93db9f3c2d1a9
praveenkumar-834 <praveenuu34@gmail.com>
2026-03-14 19:51:11 +0530

    loginuser routes added

--- controllers/authController.js
+++ controllers/authController.js
@@ -91,3 +91,40 @@ error:error.message
 }
 
 }
+
+// login user
+export const loginUser = async (req,res)=>{
+
+try{
+
+const {email,password} = req.body
+
+const user = await User.findOne({email})
+
+if(!user){
+return res.status(404).json({
+message:"User not found"
+})
+}
+
+const isMatch = await bcrypt.compare(password,user.password)
+
+if(!isMatch){
+return res.status(400).json({
+message:"Invalid password"
+})
+}
+
+res.json({
+message:"Login successful"
+})
+
+}catch(error){
+
+res.status(500).json({
+error:error.message
+})
+
+}
+
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,15 @@ import express from "express"
 import {
 registerUser,
 forgotPassword,
-resetPassword
+resetPassword,
+loginUser,
 } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post("/register",registerUser)
+
+router.post("/login",loginUser)
 
 router.post("/forgot-password",forgotPassword)
 
